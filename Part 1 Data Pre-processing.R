d2871741cd86cd3b118cf5f1cfe2c585c5db4400
Jin <jinzhao0822@gmail.com>
2019-03-14 23:16:55 +0000

    Require_14Mar2019

---
+++ Part 1 Data Pre-processing.R
@@ -0,0 +1,65 @@
+# My guidance for DMA coursework
+
+#################################################
+# Data Set  #################################################
+
+# Column - Attributes: x  ; Class/Type: 1
+# Instance: 724
+# Some missing values
+
+
+#################################################
+# Software use & Main Requirement #################################################
+
+# Only R and WEKA 
+# 20 pages includes tables & figures
+# 2000-3000 Report to illustrate the follow with well structured: 
+# !!! [What] [How] [Why] you did ?
+# Codes and row data is only for refenrence
+
+# ??? Do we need / could use other tools to make it beautiful or something?
+# what does the
+
+#################################################
+# Criteira ！！！#################################################
+
+# 1. Correctness: 
+# 用正确的方法，做出正确的假设，合理的解释数据结果。
+
+# 2.Completeness:
+# 考虑多种可能性。用多种方法、对多层次的sub数据集进行多层次的分析
+
+# 3.Originality:
+# 是否把方法们进行了新的&有趣的组合去处理数据？
+# 是否有什么新的、有趣的发现、得出了新的结论？
+
+# 4.Argumentation:
+# 你是否【4.1 解释】及【4.2批判】了你做的所有的选择？
+
+
+
+#################################################
+#Part 1 for 35 marks (46%) #################################################
+
+# 1. describing and visualizing the data
+# 2. preparing the data for subsequent treatment (‘pre- processing’)
+
+# a) Explore the data (5')
+# a1. a Table for centrality & dispersion & missing values of each
+
+
+
+
+
+#################################################
+#Part 2 Clustering for 20 marks (27%) #################################################
+
+
+
+
+#################################################
+#Part 3 Classification for 20 marks (27%) #################################################
+
+
+
+#################################################
